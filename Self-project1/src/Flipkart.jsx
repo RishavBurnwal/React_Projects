a64a2b2e18f22dc6a64e3374197f265fa5556dcd
Rishav Burnwal <burnwalrishav8@gmail.com>
2025-09-23 13:56:49 +0530

    update commit

--- Self-project1/src/Flipkart.jsx
+++ Self-project1/src/Flipkart.jsx
@@ -1,10 +1,34 @@
-import React from 'react'
+import { useEffect, useState } from "react";
+import "./Flipkart.css";
 
-function Flipkart(){
-    return (
-        <>
-        
-        </>
-    )
+function Flipkart() {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    fetch("/flipkart.json")
+      .then((res) => res.json())
+      .then((data) => setItems(data));
+  }, []);
+
+  return (
+    <>    
+    <div>
+      <h1>Flipkart Products</h1>
+      <div>
+        {items.map((item) => (
+          <div className="card">
+            <img src={item.image} alt={item.name} />
+            <h2>{item.name}</h2>
+            <p>Model : {item.model}</p>
+            <p>Offer Price : {item.price}</p>
+            <p>Ratings : {item.rating}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+    </>
+
+  );
 }
-export default Flipkart
+
+export default Flipkart;
