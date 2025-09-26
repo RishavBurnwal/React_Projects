3e6b21659750ca9cd843608a04d9a9403ab5fd32
Rishav Burnwal <burnwalrishav8@gmail.com>
2025-09-26 20:24:07 +0530

    update commit

---
+++ shopping-app/src/AdminLogin.jsx
@@ -0,0 +1,16 @@
+import React, {useState} from 'react'
+import '../Styles/AdminLogin.css'
+import { toast } from 'react-toastify'
+
+const AdminLogin = () => {
+    function val_login(e){
+        
+    }
+  return (
+    <div>
+      <h1>Admin Login</h1>
+    </div>
+  )
+}
+
+export default AdminLogin
