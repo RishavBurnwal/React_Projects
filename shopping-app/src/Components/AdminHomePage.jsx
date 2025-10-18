d2c6d426f31d367724ac0ee6c33be2e10085f725
Rishav Burnwal <burnwalrishav8@gmail.com>
2025-10-18 23:30:09 +0530

    new commit

--- shopping-app/src/Components/AdminHomePage.jsx
+++ shopping-app/src/Components/AdminHomePage.jsx
@@ -3,7 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import NavBar from './NavBar';
 import AddProducts from './AddProducts';
 import Dashboard from './Dashboard';
-import ViewProduct from './ViewProduct'; // ✅ You forgot this line
+import ViewProduct from './ViewProduct'; 
 import UpdateProduct from './UpdateProduct';
 
 function AdminHome() {
@@ -12,13 +12,9 @@ function AdminHome() {
       <NavBar />
 
       <Routes>
-        {/* Default route: Dashboard is shown immediately when admin logs in */}
+
         <Route index element={<Dashboard />} />
-
-        {/* Add Products page */}
         <Route path="add-products" element={<AddProducts />} />
-
-        {/* View Product by ID */}
         <Route path="viewProducts/:id" element={<ViewProduct />} />
         <Route path="/updateproduct/:id" element={<UpdateProduct/>}/>
       </Routes>
